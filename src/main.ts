de9508e24572434a97b83ff9f1d48346588971ab
Igor Terriaga <igsantos1996@gmail.com>
2024-04-09 14:17:58 -0300

    add transaction

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { NotFoundErrorFilter } from './not-found-error/not-found-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+  app.useGlobalPipes(new ValidationPipe({
+    errorHttpStatusCode: 422,
+    transform: true
+  }));
 
   app.useGlobalFilters(new NotFoundErrorFilter());
   await app.listen(3000);
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { NotFound } from 'src/errors';
 
